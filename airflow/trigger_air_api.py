07e5171c7d3ed3eacbfc1ce4fd57f23171a25687
Prash099 <prashanthtech099@gmail.com>
2023-06-16 12:57:50 +0200

    Add Correction Airflow Trigger Dag

--- airflow/trigger_air_api.py
+++ airflow/trigger_air_api.py
@@ -3,25 +3,23 @@ import requests
 import json
 import datetime
 import os
-from Py_Scripts import valid_output_great_expectations
 
 
 def trigger_air_api():
-    base_filename = datetime.datetime.now().strftime("%Y%m%d")
+    folder_path = '/Users/kiruba/Documents/GitHub/EPITA_DATA_SCIENCE_IN_PRODUCTION/airflow/data/Valid_Data'
 
-    for root, _, files in os.walk(valid_output_great_expectations):
-        for filename in files:
-            if filename.endswith('.csv'):
-                csv_file_path = os.path.join(root, filename)
-                
-                data_file = pd.read_csv(csv_file_path)
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.csv'):
+            file_path = os.path.join(folder_path, filename)
+            df = pd.read_csv(file_path) 
+            endpoint = 'http://127.0.0.1:8000/predict'
+            headers = {"Content-Type": "application/json"}
+            data_file = df.drop(['quality','Id'], axis =1)
 
-                endpoint = 'http://127.0.0.1:8000/predict'
-                headers = {"Content-Type": "application/json"}
-                data_file = data_file.drop(['quality','Id'], axis =1)
-
-                response = requests.post(endpoint, data=json.dumps(data_file.values.tolist()), headers=headers)
-                if response.status_code == 200:
-                    print("DAG triggered successfully.")
-                else:
-                    print("Failed to trigger DAG.")
+            response = requests.post(endpoint, data=json.dumps(data_file.values.tolist()), headers=headers)
+            if response.status_code == 200:
+                print("DAG triggered successfully.")
+            else:
+                print("Failed to trigger DAG.")
+                    
+# trigger_air_api()
